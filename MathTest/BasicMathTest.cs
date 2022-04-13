d0276e0e905fa44ba043ef44593354fed09f7b23
Snapppp <asmer1802@hotmail.com>
2022-04-13 11:01:44 +0200

    initial commit

---
+++ MathTest/BasicMathTest.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace MathTest
+{
+    [TestFixture]
+    public class BasicMathTest
+    {
+        private const double f = 1.1;
+        int i = 1 + 1;
+
+        [Test]
+        public void TestMethod1()
+        {
+            Assert.IsTrue(i == f);
+        }
+    }
+}
---
+++ MathTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MathTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MathTest")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9822295b-7b66-479b-8ec3-5f46601e2947")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
